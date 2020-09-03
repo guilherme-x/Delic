b63503bf2df3f0ab138c864e5b432e643de288af
guilherme-x <guilherme.kaliter@gmail.com>
2020-09-02 23:45:16 -0300

    first commit

---
+++ fb.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDZUf06v3QYADysSKddp0jQGFkGbywMdXI",
+    authDomain: "delic-a408f.firebaseapp.com",
+    databaseURL: "https://delic-a408f.firebaseio.com",
+    projectId: "delic-a408f",
+    storageBucket: "delic-a408f.appspot.com",
+    messagingSenderId: "925255495644",
+    appId: "1:925255495644:web:00d17d6d5533de1b59a919",
+    measurementId: "G-NH5ZWRFF63"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
